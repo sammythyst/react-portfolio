40504b28efdbd353a071eca05c7218adfb3d702d
sasw <saswain96@gmail.com>
2023-02-16 21:29:40 -0600

    cleaned up link code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,19 +20,15 @@ function Navbar() {
           textAlign: 'center',
         }}>        
         <div>
-          {/* <a href="/react-portfolio/">About Me</a> */}
           <Link to='/'>About Me</Link>
         </div>
         <div>
-          {/* <a href="/react-portfolio/portfolio">Portfolio</a> */}
           <Link to='/portfolio'>Portfolio</Link>
         </div>
         <div>
-          {/* <a href="/react-portfolio/contact">Contact</a> */}
           <Link to='/contact'>Contact</Link>
         </div>
         <div>
-          {/* <a href="/react-portfolio/resume">Resume</a> */}
           <Link to='/resume'>Resume</Link>
         </div>
       </section>
